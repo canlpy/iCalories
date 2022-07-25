54a4c2d0fdab046d230db913b1579f6692a471c7
CNLPY <calpay@yahoo.com>
2022-07-25 15:02:59 +0300

    created data model, add view, data controller file

---
+++ iCalories/Views/AddFoodView.swift
@@ -0,0 +1,47 @@
+//
+//  AddFoodView.swift
+//  iCalories
+//
+//  Created by Can on 25.07.2022.
+//
+
+import SwiftUI
+
+struct AddFoodView: View {
+    @Environment(\.managedObjectContext) var managedObject
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var calories: Double = 0
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Food name", text: $name)
+                
+                VStack {
+                    Text("Calories: \(Int(calories))")
+                    Slider(value: $calories, in: 0...1000, step: 10)
+                }
+                .padding()
+                
+                HStack{
+                    Spacer()
+                    Button("Submit") {
+                        DataController().addFood(name: name, calories: calories, context: managedObject)
+                        dismiss()
+                        
+                    }
+                    Spacer()
+                    
+                }
+            }
+        }
+    }
+}
+
+struct AddFoodView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddFoodView()
+    }
+}
--- iCalories/Views/ContentView.swift
+++ iCalories/Views/ContentView.swift
@@ -6,8 +6,10 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
