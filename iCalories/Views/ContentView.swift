23343c9392bd3bfb82b6ad53993e0cdbe6647cf3
CNLPY <calpay@yahoo.com>
2022-07-25 15:23:44 +0300

    working on contentView

--- iCalories/Views/ContentView.swift
+++ iCalories/Views/ContentView.swift
@@ -9,10 +9,45 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var managedObjContext
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.date, order: .reverse)]) var food:
+    FetchedResults<Food>
+    
+    @State private var showingAddView = false
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("\(Int(totalCaloriesToday())) Kcal (Today)")
+                    .foregroundColor(.gray)
+                    .padding(.horizontal)
+                List {
+                    ForEach(food) { food in
+                        NavigationLink(destination: Text("\(food.calories)")) {
+                            HStack {
+                                VStack(alignment: .leading, spacing: 6) {
+                                    Text(food.name!)
+                                        .bold()
+                                    
+                                    Text
+                                    
+                                    
+                                }
+                            }
+                        }
+
+
+                          
+                        
+                    }
+                }
+                
+            }
+            .navigationTitle("iCalories")
+        }
+    }
+    private func totalCaloriesToday() -> Double {
+        return 0.0
     }
 }
 
