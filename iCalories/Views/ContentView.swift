8f54972942baf6825afeaaee3d9a6c6b019bd742
CNLPY <calpay@yahoo.com>
2022-07-25 18:00:10 +0300

    today total calories ok. delete func working.

--- iCalories/Views/ContentView.swift
+++ iCalories/Views/ContentView.swift
@@ -29,25 +29,60 @@ struct ContentView: View {
                                     Text(food.name!)
                                         .bold()
                                     
-                                    Text
-                                    
-                                    
+                                    Text("\(Int(food.calories))") +
+                                    Text(" Calories")
+                                        .foregroundColor(.red)
                                 }
+                                Spacer()
+                                Text(calcTimeSince(date: food.date!))
+                                    .foregroundColor(.gray)
+                                    .italic()
+                                
                             }
                         }
-
-
-                          
-                        
-                    }
+                     }
+                    .onDelete(perform: deleteFood)
                 }
+                .listStyle(.plain)
+                
                 
             }
+            
             .navigationTitle("iCalories")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        showingAddView.toggle()
+                    } label: {
+                        Label("Add food", systemImage: "plus.circle")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+            }
+            .sheet(isPresented: $showingAddView) {
+                AddFoodView()
+            }
+        }
+        .navigationViewStyle(.stack)
+    }
+    private func deleteFood(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { food[$0] }
+            .forEach(managedObjContext.delete)
+            
+            DataController().save(context: managedObjContext)
         }
     }
     private func totalCaloriesToday() -> Double {
-        return 0.0
+        var caloriesToday: Double = 0
+        for item in food {
+            if Calendar.current.isDateInToday(item.date!) {
+                caloriesToday += item.calories
+            }
+        }
+        return caloriesToday
     }
 }
 
